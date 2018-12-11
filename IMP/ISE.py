ec9a9c267f9efb326926a64c3bf3eacb4aa46f68
yinzho <yinnzho@gmail.com>
2018-12-11 13:40:02 +0800

    add multiprocessing

--- IMP/ISE.py
+++ IMP/ISE.py
@@ -1,9 +1,11 @@
-import copy
+import os
+import sys
 import time
 import math
 import random
 import argparse
 from collections import defaultdict
+from multiprocessing import Pool
 
 seed_set = []
 
@@ -39,9 +41,9 @@ def read_social_network_graph(file_name):
             
             if i == 0:
                 vertices, edges = line
-                adj_list = [list() for i in range(vertices)]
-                neighbor = [list() for i in range(vertices)]
-                edge_weight = [defaultdict() for i in range(vertices)]
+                adj_list = [list() for i in range(vertices+1)]
+                neighbor = [list() for i in range(vertices+1)]
+                edge_weight = [defaultdict() for i in range(vertices+1)]
             else:
                 u,v,w = line
                 adj_list[u].append(v)
@@ -51,102 +53,94 @@ def read_social_network_graph(file_name):
     return Graph(vertices, edges, edge_weight, adj_list, neighbor)
     
 
-def LT_model(graph: Graph, time_budget):
+def LT_model(time_budget):
     curtime = time.time()
-
-    N = 0
     sum = 0
+    N = 0
     last_time = 0
     while True:
-        star_time = time.time()
+        startime = time.time()
+        if time.time() - curtime + last_time > time_budget - 1 or N >= 10000/workers:
+            break
 
-        activity_set = copy.deepcopy(seed_set)
-        thresholds = [0 for i in range(graph.vertices)]
-        isActive = [False for i in range(graph.vertices)]
+        activity_set = seed_set
+        thresholds = [0 for i in range(graph.vertices+1)]
+        isActive = set()
 
         # initialize threshold
-        for i in range(len(thresholds)):
-
-            rand_num = random.random()
-            thresholds[i] = rand_num
-            if rand_num == 0:
-                activity_set.append(i)
+        for i in range(len(thresholds)):          
+            if i == 0:
+                thresholds[i] = math.inf
+            else:
+                rand_num = random.random()
+                thresholds[i] = rand_num
+                if rand_num == 0:
+                    activity_set.append(i)
 
         count = len(activity_set)   
 
         # initialize isActive      
         for e in activity_set:
-            isActive[e] = True
-                
-
+            isActive.add(e)             
         while activity_set:
             new_activity_set = []
             for seed in activity_set:
                 for u in graph.adj_list[seed]:
-                    if isActive[u] is False:
+                    if u not in isActive:
                         w_total = 0
-                        for n in graph.neighbor[u]:
-                            if isActive[n] is True:
-                                w_total += graph.edge_weight[n][u]
+                        for neighbor in graph.neighbor[u]:
+                            if neighbor in isActive:
+                                w_total += graph.edge_weight[neighbor][u]
                         if w_total >= thresholds[u]:
-                            isActive[u] = True
+                            isActive.add(u)
                             new_activity_set.append(u)
             count += len(new_activity_set)
-            activity_set = copy.deepcopy(new_activity_set)
-        # print(count)
-        N += 1
-        sum += count
+            activity_set = new_activity_set
 
-        last_time = time.time() - star_time
-        if last_time + time.time() - curtime > time_budget - 1:
-            break  
-    print('N: '+str(N))
+        N += 1
+        sum += count 
+        last_time = time.time() - startime
     return sum / N
     
 
-def IC_model(graph: Graph, time_budget):
+def IC_model(time_budget):
     curtime = time.time()
-
-    N = 0
     sum = 0
+    N = 0
     last_time = 0
     while True:
-        star_time = time.time()
+        startime = time.time()
+        if time.time() - curtime + last_time > time_budget - 1 or N >= 10000/workers:
+            break
 
-        activity_set = copy.deepcopy(seed_set)
-        isActive = [False for i in range(graph.vertices)]
-
+        activity_set = seed_set
+        isActive = set()
         # initialize isActive      
         for e in activity_set:
-            isActive[e] = True
+            isActive.add(e)
     
         count = len(activity_set)
 
-        while activity_set:
+        while len(activity_set) > 0:
             new_activity_set = []
             for seed in activity_set:
                 for u in graph.adj_list[seed]:
-                    if isActive[u] is False:
-                        rand_num = random.random()
-                        if rand_num <= graph.edge_weight[seed][u]:
-                            isActive[u] = True
+                    if u not in isActive and random.uniform(0, 1) <= graph.edge_weight[seed][u]:
+                            isActive.add(u)
                             new_activity_set.append(u)
             count += len(new_activity_set)
-            activity_set = copy.deepcopy(new_activity_set)
+            activity_set = new_activity_set
         
         N += 1
-        sum += count
-        last_time = time.time() - star_time
-        if last_time + time.time() - curtime > time_budget - 1:
-            break  
-    print('N: '+str(N))
+        sum += count 
+        last_time = time.time() - startime
+
     return sum / N
         
 
 
 if __name__ == "__main__":
     curtime = time.time()
-
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', dest='social_network' ,help='the absolute path of the social network file')
     parser.add_argument('-s', dest='seed_set', help='the absolute path of the seed set file')
@@ -155,13 +149,21 @@ if __name__ == "__main__":
     parse_res = parser.parse_args()
     
     read_seed_set(parse_res.seed_set)
+    
+    global workers
+    workers = 8
+    global graph
     graph = read_social_network_graph(parse_res.social_network)
+
     if parse_res.diffusion_model == 'IC':
-        print(str(IC_model(graph, parse_res.time_budget - (time.time() - curtime))))
+        with Pool(workers) as p:
+            res = p.map(IC_model, [int(parse_res.time_budget) - (time.time() - curtime) for i in range(workers)]) 
+            print(sum(res) / workers)
     elif parse_res.diffusion_model == 'LT':
-        print(str(LT_model(graph, parse_res.time_budget - (time.time() - curtime))))
-    
-    print(str(time.time() - curtime))
-    
-    
+        with Pool(workers) as p:
+            res = p.map(LT_model, [int(parse_res.time_budget) - (time.time() - curtime) for i in range(workers)]) 
+            print(sum(res) / workers)
+    # print(time.time() - curtime)
+    sys.stdout.flush()
+    os._exit(0)
     
